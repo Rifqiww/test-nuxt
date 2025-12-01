92306210e21e9d9674251a133ac8a2154883d88e
Rifqiww <rifqirafiudin569@gmail.com>
2025-12-01 16:11:29 +0700

    📝add: nuxt, pinia, primevue, and tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,20 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
+  css: ["./app/assets/css/tailwind.css"],
   devtools: { enabled: true },
-
+  vite: {
+    plugins: [tailwindcss()],
+  },
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/hints',
-    '@nuxt/image',
-    '@nuxt/ui'
-  ]
-})
+    "@nuxt/content",
+    "@nuxt/eslint",
+    "@nuxt/hints",
+    "@nuxt/image",
+    "@nuxt/ui",
+    "@pinia/nuxt",
+    "@primevue/nuxt-module",
+  ],
+});
